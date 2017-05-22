c89e8e19d4c4e1dfe4ad7990d83aeced66b3aeb5
Luke Rees <escrew7@hotmail.com>
2017-05-22 18:19:45 +0800

    update server.js again

--- server.js
+++ server.js
@@ -5,15 +5,15 @@ var app = express();
 const port = process.env.PORT || 3000;
 
 app.use(function (req, res, next){
-  if (req.headers['x-fowarded-proto'] === 'http') {
+  if (req.headers['x-forwarded-proto'] === 'http') { // <------ difference here
     next();
   } else {
-    res.redirect('http://' + req.hostname + req.url)
+    res.redirect('http://' + req.hostname + req.url);
   }
 });
 
 app.use(express.static('public'));
 
 app.listen(port, function () {
-    console.log('Express server is up on port ' + port);
+    console.log('Express server is up on port 3000');
 });
