8a4f1cdc1b7e3df2ee2965a4708ee7a5332b5b0c
Luke Rees <escrew7@hotmail.com>
2017-05-29 17:16:10 +0800

    styled about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,8 +3,17 @@ var React = require('react');
 var About = (props) => {
   return (
     <div>
-      <h3>About</h3>
+      <h1 className="text-center">About</h1>
       <p>Welcome to the About Page</p>
+      <p>This weather app has been made with the Udemy Course <a href="https://www.udemy.com/the-complete-react-web-app-developer-course/learn/v4/overview"> The
+      Complete React Developers Course</a> by Andrew Mead.</p>
+      <p>This app is generated using the <a href="https://facebook.github.io/react/">React</a> Engine and
+      utilises the <a href="https://openweathermap.org/">OpenWeatherMap</a> API.
+      </p>
+      <p>If you wish to delve deeper into the code please visit my <a href="https://github.com/escrew7/ReactWeather">github
+      repository.</a></p>
+      <p>Thanks for your time!</p>
+      <p>Luke Rees</p>
     </div>
   )
 }
